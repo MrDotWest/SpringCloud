8d354eb5e58610cbc2db0b8111e993f246f0be24
MrDotWest <287322940@qq.com>
2020-05-04 11:19:17 +0800

    提交代码

---
+++ GateWay/src/main/java/com/wufl/gateway/DynamicRouteServiceImpl.java
@@ -0,0 +1,50 @@
+package com.wufl.gateway;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.event.RefreshRoutesEvent;
+import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.cloud.gateway.route.RouteDefinitionWriter;
+import org.springframework.cloud.gateway.support.NotFoundException;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.ApplicationEventPublisherAware;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+@Service
+public class DynamicRouteServiceImpl implements ApplicationEventPublisherAware {
+
+    @Autowired
+    private RouteDefinitionWriter routeDefinitionWriter;
+    private ApplicationEventPublisher publisher;
+
+    @Override
+    public void setApplicationEventPublisher(ApplicationEventPublisher applicationEventPublisher) {
+        this.publisher = applicationEventPublisher;
+    }
+
+    public String add(RouteDefinition definition) {
+        routeDefinitionWriter.save(Mono.just(definition)).subscribe();
+        this.publisher.publishEvent(new RefreshRoutesEvent(this));
+        return "success";
+    }
+
+    public String update(RouteDefinition definition) {
+        try {
+            delete(definition.getId());
+        } catch (Exception e) {
+            return "update fail,not find route  routeId: "+definition.getId();
+        }
+        try {
+            routeDefinitionWriter.save(Mono.just(definition)).subscribe();
+            this.publisher.publishEvent(new RefreshRoutesEvent(this));
+            return "success";
+        } catch (Exception e) {
+            return "update route  fail";
+        }
+    }
+    //删除路由
+    public Mono<ResponseEntity<Object>> delete(String id) {
+        return this.routeDefinitionWriter.delete(Mono.just(id)).then(Mono.defer(() -> Mono.just(ResponseEntity.ok().build()))).onErrorResume((t) -> t instanceof NotFoundException, (t) -> Mono.just(ResponseEntity.notFound().build()));
+    }
+}
